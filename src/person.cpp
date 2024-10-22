9996bb54f2146b299bb026d8c9788261a7c2c6bb
Alexander Pillai <pillai.alexander.n@gmail.com>
2024-10-22 16:40:43 -0500

    adding vaccine protection to probability of infection
    vaccine effect is a multiplicative reduction in the susceptibility of
    the individual. if there is no vaccine effect or the vaccine is modeled
    via the baseline susceptibility distribution, the vaccine effect should
    be equal to 0 such that the effect on susceptibility is 1-0=1 (ie no
    effect since 1*susceptibility=susceptibility)

--- src/person.cpp
+++ src/person.cpp
@@ -35,7 +35,10 @@ double Person::get_susceptibility() const { return susceptibility; }
 void Person::set_susceptibility(double s) { susceptibility = s; }
 
 bool Person::infect(StrainType strain, size_t time) {
-    if (rng->draw_from_rng(INFECTION) < susceptibility) {
+    auto current_suscep = susceptibility;
+    current_suscep *= is_vaccinated() ? 1 - vaccine_protection : 1;
+
+    if (rng->draw_from_rng(INFECTION) < current_suscep) {
         auto sympt = (rng->draw_from_rng(INFECTION) < par->pr_symptoms[strain]) ? SYMPTOMATIC : ASYMPTOMATIC;
         auto seek_care = rng->draw_from_rng(BEHAVIOR) < par->pr_seek_care[vaccination_status];
         infection_history.push_back(std::make_unique<Infection>(strain, time, sympt, seek_care));
