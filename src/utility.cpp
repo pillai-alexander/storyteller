00d1d8104292455a1374a18754686b73bce83076
Alexander Pillai <pillai.alexander.n@gmail.com>
2024-10-25 16:29:29 -0500

    adding util function to calc gamma scale from shape and mean

--- src/utility.cpp
+++ src/utility.cpp
@@ -3,3 +3,9 @@
 namespace constants {
     unsigned int ONE = 1;
 }
+
+namespace util {
+    double gamma_scale_from_mean(double shape, double mean) {
+        return mean / shape;
+    }
+}
--- src/utility.hpp
+++ src/utility.hpp
@@ -8,3 +8,7 @@ namespace constants {
 
 template<typename T> using vector2d = std::vector<std::vector<T>>;
 template<typename T> using vector3d = std::vector<std::vector<std::vector<T>>>;
+
+namespace util {
+    extern double gamma_scale_from_mean(double shape, double mean);
+}
