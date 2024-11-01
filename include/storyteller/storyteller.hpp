f604c6868f665a9fd1761ea9a02afbc6d1ff5f12
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2024-11-01 12:12:07 -0500

    documenting storyteller files

--- include/storyteller/storyteller.hpp
+++ include/storyteller/storyteller.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file storyteller.hpp
+ * @author Alexander N. Pillai
+ * @brief Contains the Storyteller class that is responsible for taking in user
+ *        input and performing the desired operations (eg, database initialization,
+ *        simulations)
+ *
+ * @copyright TBD
+ *
+ */
 #pragma once
 
 #include <map>
@@ -11,6 +21,11 @@ class DatabaseHandler;
 class RngHandler;
 class Parameters;
 
+/**
+ * @brief Defines the types of operations that the Storyteller is capable of
+ *        performing.
+ *
+ */
 enum OperationType {
     PROCESS_CONFIG,
     EXAMPLE_SIM,
@@ -18,40 +33,138 @@ enum OperationType {
     NUM_OPERATION_TYPES
 };
 
+/**
+ * @brief Primary object that interfaces with the user, processes user input,
+ *        and performs desired operations.
+ *
+ * A storyteller object is created by the user's main file and handles all user
+ * input (including command-line argumets and configuration files). The object
+ * then processes the user input to determine what operation is desired (currently
+ * either database construction or simulation) and performs that operation.
+ *
+ */
 class Storyteller {
   public:
+    /**
+     * @brief Construct a new Storyteller object using all user-provided input.
+     *
+     * @param argc Number of command-line arguments
+     * @param argv Array of command-line arguments
+     */
     Storyteller(int argc, char* argv[]);
     ~Storyteller();
 
+    /**
+     * @brief Get the simulation serial.
+     *
+     * @return int Simulation serial number
+     */
     int get_serial() const;
+
+    /**
+     * @brief Get the number of simulations to be run in the batch.
+     *
+     * @return size_t Simulation batch size
+     */
     size_t get_batch_size() const;
+
+    /**
+     * @brief Get the path to the configuration file.
+     *
+     * @return std::string Configuration file path
+     */
     std::string get_config_file() const;
 
+    /**
+     * @brief Get a specific program flag.
+     *
+     * @param key Name of the desired program flag as a string.
+     * @return true Flag "key" is set to true
+     * @return false Flag "key" is set to false
+     */
     bool get_flag(std::string key) const;
+
+    /**
+     * @brief Set a specific program flag.
+     *
+     * @param key Name of the desired flag as a string
+     * @param val Boolean value for flag "key"
+     */
     void set_flag(std::string key, bool val);
 
+    /**
+     * @brief Run the user-desired operation.
+     *
+     * @return int Return code of the operation (0 if sucessful)
+     */
     int run();
   private:
+    /**
+     * @todo: process user input to determine if it matches expectations
+     *
+     * @return true
+     * @return false
+     */
     bool sensible_inputs() const;
 
+    /**
+     * @brief Initialize Storteller for running a simulation.
+     *
+     */
     void init();
+
+    /**
+     * @brief Process configuration file and construct the database if it does
+     *        not already exist.
+     *
+     * @return int Return code (0 if sucessful)
+     */
     int process_config();
+
+    /**
+     * @brief Constructs a new experiment database given the user-provided
+     *        configuration file.
+     *
+     * @return int Return code (0 if sucessful)
+     */
     int construct_database();
+
+    /**
+     * @brief Built-in example simulation that does not require a configuration
+     *        file.
+     *
+     * @return int Return code (0 if sucessful)
+     */
     int default_simulation();
+
+    /**
+     * @brief Runs a batch of simulations drawn from an experiment database.
+     *
+     * @return int Return code (0 if sucessful)
+     */
     int batch_simulation();
 
+    /**
+     * @brief Calls the Rscript to generate the simulation dashboard.
+     *
+     * @return int Return code (0 if sucessful)
+     */
     int draw_simvis();
 
+    /**
+     * @brief Resets the Storteller before a new simulation.
+     *
+     */
     void reset();
 
-    std::unique_ptr<Simulator> simulator;
-    std::unique_ptr<DatabaseHandler> db_handler;
-    std::unique_ptr<RngHandler> rng_handler;
-    std::unique_ptr<Parameters> parameters;
+    std::unique_ptr<Simulator> simulator;           ///< Created for each simulation to be run
+    std::unique_ptr<DatabaseHandler> db_handler;    ///< Handles all database operations
+    std::unique_ptr<RngHandler> rng_handler;        ///< Handles all pseudo-random number generation
+    std::unique_ptr<Parameters> parameters;         ///< Stores all necessary simulation parameters
     OperationType operation_to_perform;
 
-    argh::parser cmdl_args;
-    std::map<std::string, bool> simulation_flags;
+    argh::parser cmdl_args;                         ///< Stores parsed command-line arguments
+    std::map<std::string, bool> simulation_flags;   ///< Stores program flags
     int simulation_serial;
     size_t batch_size;
     std::string config_file;
--- src/storyteller.cpp
+++ src/storyteller.cpp
@@ -15,6 +15,10 @@
 
 using json = nlohmann::json;
 
+/**
+ * @details Parses command-line arguments and extracts all necessary program flags into
+ *          #simulation_flags.
+ */
 Storyteller::Storyteller(int argc, char* argv[])
     : simulation_serial(-1),
       batch_size(1),
@@ -22,19 +26,25 @@ Storyteller::Storyteller(int argc, char* argv[])
       simulator(nullptr),
       operation_to_perform(NUM_OPERATION_TYPES),
       simulation_flags() {
+    // parse command-line arguments using Argh! library
     cmdl_args.parse(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
 
+    /// @todo implement #sensible_inputs() to handle all checks here before moving on
+
+    // store all necessary program-flags
     simulation_flags["process"]  = cmdl_args["process"];
     simulation_flags["simulate"] = cmdl_args["simulate"];
     simulation_flags["particle"] = cmdl_args["particle"];
     simulation_flags["example"]  = cmdl_args["example"];
     simulation_flags["simvis"]   = cmdl_args["simvis"];
 
+    // extract sim serial
     if (simulation_flags["particle"] and not (cmdl_args({"-s", "--serial"}) >> simulation_serial)) {
         std::cerr << "ERROR: pass serial id file after --particle using -s or --serial.";
         exit(-1);
     }
 
+    // extract batch size if present or default to one (ie, a single simulation batch)
     cmdl_args({"-b", "--batch"}, 1) >> batch_size;
 
     if ((simulation_flags["process"] or simulation_flags["particle"]) and not (cmdl_args({"-f", "--file"}) >> config_file)) {
@@ -47,6 +57,7 @@ Storyteller::Storyteller(int argc, char* argv[])
         exit(-1);
     }
 
+    // determine what operation the user called for
     if (simulation_flags["process"] and not config_file.empty()) {
         operation_to_perform = PROCESS_CONFIG;
     } else if (simulation_flags["simulate"]) {
@@ -63,18 +74,18 @@ Storyteller::Storyteller(int argc, char* argv[])
 
 Storyteller::~Storyteller() {}
 
-int Storyteller::get_serial() const { return simulation_serial; }
-size_t Storyteller::get_batch_size() const { return batch_size; }
-std::string Storyteller::get_config_file() const { return config_file; }
-
+int Storyteller::get_serial()               const { return simulation_serial; }
+size_t Storyteller::get_batch_size()        const { return batch_size; }
+std::string Storyteller::get_config_file()  const { return config_file; }
 bool Storyteller::get_flag(std::string key) const { return simulation_flags.at(key); }
+
 void Storyteller::set_flag(std::string key, bool val) { simulation_flags[key] = val; }
 
 int Storyteller::run() {
     switch (operation_to_perform) {
         case PROCESS_CONFIG: return construct_database();
-        case EXAMPLE_SIM: return default_simulation();
-        case BATCH_SIM: return batch_simulation();
+        case EXAMPLE_SIM:    return default_simulation();
+        case BATCH_SIM:      return batch_simulation();
         default: {
             std::cerr << "No operation performed.";
             return 0;
@@ -82,6 +93,12 @@ int Storyteller::run() {
     }
 }
 
+/**
+ * @details Performs a built-in simulation that uses all of the default parameter
+ *          values and that does not require any user-provided configuration nor
+ *          an experiment database. For this operation, #init default-initializes
+ *          the #db_handler, #parameters, and #rng_handler objects.
+ */
 int Storyteller::default_simulation() {
     init();
     simulator = std::make_unique<Simulator>(parameters.get(), db_handler.get(), rng_handler.get());
@@ -94,6 +111,13 @@ int Storyteller::default_simulation() {
     return 0;
 }
 
+/**
+ * @details Performs a batch of simulations that require an experiment database
+ *          for parameterization. For each simulation, the Storyteller is initialized
+ *          using the proper particle from the database (which also correctly
+ *          initializes the #db_handler, #parameters, and #rng_handler objects for
+ *          each simulation in the batch).
+ */
 int Storyteller::batch_simulation() {
     init();
     for (size_t i = 1; i <= batch_size; ++i) {
@@ -114,35 +138,57 @@ int Storyteller::batch_simulation() {
     return 0;
 }
 
+/**
+ * @details Initializes the Storyteller appropriately for the proper type of
+ *          simulation (ie, example or particle). For the example simulation,
+ *          the #db_handler, #parameters, and #rng_handler objects are default-
+ *          initialized. Otherwise, parameter values are read from the appropriate
+ *          particle in the experiment database and used to construct the
+ *          #db_handler, #parameters, and #rng_handler objects.
+ */
 void Storyteller::init() {
     if (simulation_flags["simulate"] and not config_file.empty()) {
+        // slurp the JSON configuration file
         std::ifstream cfg_file(config_file);
         auto cfg = json::parse(cfg_file);
         cfg_file.close();
 
-        std::string db_path = cfg["experiment_name"];
-        db_path += std::string(".sqlite");
+        // extract the experiment database path
+        // if the user specifies a path, use that path
+        // otherwise, the default value is a databse in the main.cpp directory
+        // that is named using the experiment name
+        std::string db_path = (cfg.contains("database_path"))
+                                  ? std::string(cfg["database_path"])
+                                  : std::string(cfg["experiment_name"]) + ".sqlite";
 
+        // init a dictionary to store parameter names and values from the
+        // experiment database
         std::map<std::string, double> model_params;
         for (auto& [key, el] : cfg["model_parameters"].items()) {
             model_params[el["fullname"]] = 0.0;
         }
 
+        // store the names of the metrics to report
         std::vector<std::string> model_mets;
         for (auto& [key, el] : cfg["metrics"].items()) {
             model_mets.push_back(el["fullname"]);
         }
+
+        // create the DatabaseHandler and read the proper parameters
         db_handler = std::make_unique<DatabaseHandler>(this, db_path);
         db_handler->read_parameters(simulation_serial, model_params);
 
+        // create the RngHandler with the proper seed
         rng_handler = std::make_unique<RngHandler>(model_params["seed"]);
 
+        // create the Parameters and update with the proper values
         parameters = std::make_unique<Parameters>(rng_handler.get(), model_params);
         parameters->simulation_duration = cfg["sim_duration"];
         parameters->database_path       = db_path;
         parameters->return_metrics      = model_mets;
         parameters->simulation_serial   = simulation_serial;
     } else {
+        // default-initialize the DatabaseHandler, RngHandler, and Parameters
         db_handler = nullptr;
         rng_handler = std::make_unique<RngHandler>(0);
         parameters = std::make_unique<Parameters>(rng_handler.get());
@@ -166,6 +212,11 @@ int Storyteller::draw_simvis() {
     return system(cmd.str().c_str());
 }
 
+/**
+ * @details Deletes the current #simulator, #db_handler, #parameters, and
+ *          #rng_handler objects after a simulation is finished so that they can
+ *          be properly initialized for the next simulation in the batch.
+ */
 void Storyteller::reset() {
     simulator.reset(nullptr);
     db_handler.reset(nullptr);
@@ -173,21 +224,32 @@ void Storyteller::reset() {
     parameters.reset(nullptr);
 }
 
+/**
+ * @details Constructs the experiment database using the user-provided configuration
+ *          file if the database does not already exist.
+ */
 int Storyteller::process_config() {
     if (simulation_flags["process"] and not config_file.empty()) {
+        // slurp the JSON configuration file
         std::ifstream cfg_file(config_file);
         auto cfg = json::parse(cfg_file);
         cfg_file.close();
 
+        // extract the experiment database path
+        // if the user specifies a path, use that path
+        // otherwise, the default value is a databse in the main.cpp directory
+        // that is named using the experiment name
         std::string db_path = (cfg.contains("database_path"))
                                   ? std::string(cfg["database_path"])
                                   : std::string(cfg["experiment_name"]) + ".sqlite";
 
+        // create the DatabaseHandler
         db_handler = std::make_unique<DatabaseHandler>(this, db_path);
         if (db_handler->database_exists()) {
             std::cerr << "ERROR: database " << db_path << " already exists\n";
             return -1;
         } else {
+            // construct the experiment database since it does not exist
             std::cerr << db_path << " does not exist. Initializing...\n";
             return db_handler->init_database(cfg);
         }
