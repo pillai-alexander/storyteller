07d25072ccbd8f14fe5c42e72c9dc3afe0c53049
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2024-12-18 10:48:57 -0600

    adding util functions related to exp decay
    one calculates the correct decay rate given some half life
    
    the other calculates the %decay for some rate and time point

--- include/storyteller/utility.hpp
+++ include/storyteller/utility.hpp
@@ -62,6 +62,9 @@ namespace util {
 
     extern double logistic(const double log_odds);
     extern double logit(const double prob);
+
+    extern double exp_decay_rate_from_half_life(const double half_life);
+    extern double exp_decay(const double rate, const double time);
 }
 
 /**
