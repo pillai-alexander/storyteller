ee87ff80fea820f225fc98a999de51150f05432a
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2024-12-19 15:24:04 -0600

    Adding sensible descriptions and validations to default parameters (#20)
    * add a parameter dictionary to the sim report
    
    this prints the fullname, nickname, and description to the report for
    all parameters
    
    * adding default parameter descriptions
    
    * minor report edit
    
    * additional param template comments
    
    * more sensible default parameter validations
    
    the default validation functions should be as general as possible:
    
    - any probability has validation that just checks that the value is
    within (0,1)
    - all suscep values should be within (0,1) now that the sampling scheme
    uses beta-like distributions for susceptibility
    - all variances must be greater than 0
    - all vax efficacy values should be within (0,1)
    - sim length and pop size should be greater than 0
    
    the user can define more restrictive validations if desired

--- src/storyteller.cpp
+++ src/storyteller.cpp
@@ -300,7 +300,7 @@ int Storyteller::generate_exp_report() {
     }
     report << '\n';
 
-    report << "## Const parameters:\n"
+    report << "## Constant parameters:\n"
            << '\n'
            << "Name | Value\n"
            << "--- | ---\n";
@@ -319,6 +319,23 @@ int Storyteller::generate_exp_report() {
         const auto p = par_table.get<sol::table>(name);
         const auto who = p.get<std::string>("who");
         report << name << " | " << who << '\n';
+    }
+    report << '\n';
+
+    report << "## Parameter dictionary:\n\n";
+    for (const auto& [flag, pars] : par_names) {
+        for (const auto& name : pars) {
+            const auto p = par_table.get<sol::table>(name);
+
+            auto fullname = name;
+            std::replace(fullname.begin(), fullname.end(), '_', ' ');
+            const auto nickname = p.get<std::string>("nickname");
+            const auto desc = p.get<std::string>("description");
+
+            report << "**" << fullname << " (" << nickname << ")**\n"
+                   << "> " << desc << '\n'
+                   << '\n';
+        }
     }
     report << '\n';
     report.close();
